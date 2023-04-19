ed691d7f2cc050f0f0d6530c9b6975bd870de584
Javier Vargas <3231javiervargas@gmail.com>
2023-04-18 21:38:41 -0400

    user auth

--- doobbile_app/__init__.py
+++ doobbile_app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 from doobbile_app.secrets import SECRET_KEY
+
 
 
 app = Flask(__name__)
@@ -9,6 +12,10 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = SECRET_KEY
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 db = SQLAlchemy(app)
 
--- doobbile_app/models.py
+++ doobbile_app/models.py
@@ -1,13 +1,19 @@
 from datetime import datetime
-from doobbile_app import db
+from doobbile_app import db, login_manager
+from flask_login import UserMixin
 
-class User(db.Model):
-    # Hash the Id
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
+    # Remember hash the Id
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=False,
-                           default='default="default.jpg')
+                           default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
 
@@ -18,8 +24,9 @@ class User(db.Model):
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    # Set up for svg text 
+    date_posted = db.Column(db.DateTime, nullable=False,
+                            default=datetime.utcnow)
+    # Set up for svg text
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
 
